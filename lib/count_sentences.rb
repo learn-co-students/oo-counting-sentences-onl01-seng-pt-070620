221b3cd1c673e78965774abbba3fb6b58f36eab1
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-24 00:18:52 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+   # if self.end_with?(".")
+     # true
+   # else false
+   # end
+   
+   #self.end_with?(".")? true:false
+ 
   end
 
   def question?
-
+  self.end_with?("?")
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+    sentences=self.split(/[.?!]/).reject {|string| string.empty?}
+    sentences.count
+end
 end
